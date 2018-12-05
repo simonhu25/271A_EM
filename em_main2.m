59f96b6509b64bc778fe4b2e2207ac28ce67c3c7
Simon Hu <simonhu@ieee.org>
2018-12-05 15:19:02 -0800

    Updated problem 2.

---
+++ em_main2.m
@@ -0,0 +1,24 @@
+% Written by Jun Hao Hu, University of California San Diego.
+% All rights reserved.
+
+%---------------------------------------------------------------------
+% Main MATLAB routine for homework 4, part 2.
+%---------------------------------------------------------------------
+%% Load data and define parameters. 
+load('load_data.mat'); % MATLAB data array that contains information already loaded in the first section of em_main.
+
+classes = [1 2 4 8 16 32];
+n_classes = size(classes,2);
+p_error = zeros(n_classes,n_dims);
+fprintf('Loading completed. Training and predicting models ...\n');
+
+%% EM model training and prediction.
+parfor idx_class = 1:n_classes
+   [mu_fg_c,sigma_fg_c,pi_fg_c] = em_calc(dct_coeffs,classes(idx_class),M,200);
+   [mu_bg_c,sigma_bg_c,pi_bg_c] = em_calc(dct_coeffs,classes(idx_class),M,200);
+   for idx_dim = 1:n_dims
+       A = bdr_predict(dct_coeffs,dims(idx_dim),mu_fg_c,mu_bg_c,sigma_fg_c,sigma_bg_c,...
+           pi_fg_c,pi_bg_c,p_fg,p_bg,rows_cheetah_ori,cols_cheetah_ori,classes(idx_class));
+       p_error(idx_class,idx_dim) = calc_error(A,p_fg,p_bg);
+   end
+end
